ba898ba376469f386e15964fa6299c756a23bd24
SHjNEe <baotrung.jpvn@gmail.com>
2022-10-26 01:39:45 +0700

    Add

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -8,14 +8,3 @@ mongoose.connect(
   }
 );
 
-const Task = mongoose.model("Task", {
-  description: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-});
